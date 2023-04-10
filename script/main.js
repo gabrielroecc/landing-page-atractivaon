fdf8704b94ab0ba38c10241c75d7f2850b26e397
Gabriel Roec <104526610+gabrielroecc@users.noreply.github.com>
2023-04-10 15:33:07 -0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
